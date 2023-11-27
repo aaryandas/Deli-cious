d0290de1b70022f3af2d7989df9f5bf8c5bf9b33
nadia <nadiaandu7@gmail.com>
2023-11-27 13:33:08 -0600

    update

--- DeliSandwichProject/src/main/java/org/example/UserInterface.java
+++ DeliSandwichProject/src/main/java/org/example/UserInterface.java
@@ -4,12 +4,12 @@ import java.util.Scanner;
 
 public class UserInterface {
 
-    public void displayHomeScreen(){
+    public void displayHomeScreen() {
 
         boolean isRunning = true;
-        while(isRunning){
-            try{
-                System.out.println("Welcome to the Sandwich Store!");
+        while (isRunning) {
+            try {
+                System.out.println("Welcome to Sobway! Sandwiches so good you can't help but cry!");
                 System.out.println("Please choose one of the following options: ");
                 System.out.println("1) Place a new order");
                 System.out.println("0) Exit the application");
@@ -17,50 +17,121 @@ public class UserInterface {
                 Scanner scnr = new Scanner(System.in);
                 int choice = scnr.nextInt();
 
-                switch(choice){
+                switch (choice) {
                     case 1:
                         displaySandwichScreen();
                         break;
                     case 0:
-                        System.out.println("Thank you! Please come again!");
+                        System.out.println("Thank you for dining with us! Please come again!");
                         System.exit(0);
                         break;
                 }
-
-            }
-            catch(Exception ex){
+            } catch (Exception ex) {
                 System.out.println("Invalid input. Please try again");
             }
         }
-     }
+    }
 
-    private void displayOrderScreen(){
+    private void displayOrderScreen() {
+        // when they have completed the order, application should display order details
+        // including the list of sandwiches that were ordered to verify the order is correct
         System.out.println("********** ORDER SCREEN **********");
-        System.out.println();
-     }
+        System.out.println("1. Sandwich");
+        System.out.println("2. Add-ons");
+        System.out.println("3. Drink");
+        System.out.println("4. Chips");
+        System.out.println("5. Checkout");
+        System.out.println("6. Cancel order");
 
-    private void exitProgram(){}
+        Scanner scanner = new Scanner(System.in);
+        int choice = scanner.nextInt();
 
-    private void displaySandwichScreen(){
-        System.out.println("sandwich screen working correctly");
+        switch (choice) {
+            case 1:
+                displaySandwichScreen();
+                break;
+            case 2:
+                displayAddOnScreen();
+                break;
+            case 3:
+                displayDrinkScreen();
+                break;
+            case 4:
+                displayChipsScreen();
+                break;
+            case 5:
+                displayCheckoutScreen();
+                break;
+            case 6:
+                cancelOrder();
+                break;
+            case 7:
+                System.out.println("Invalid selection, choose from options listed above please.");
+                displayOrderScreen();
+        }
+
     }
 
-    private void displayAddOnScreen(){
+    private void exitProgram() {
+    }
+
+    private void displaySandwichScreen() {
+        System.out.println("Select sandwich size");
+        System.out.println("Select bread type");
+        System.out.println("Toasted or untoasted?");
+    }
+
+    private void displayAddOnScreen() {
         System.out.println("********** ADD ONS **********");
-        System.out.println();
+        System.out.println("Add regular toppings");
+        System.out.println("Add premium toppings");
     }
 
-    private void displayDrinkScreen(){
-        System.out.println("drink screen working");
+    private void displayDrinkScreen() {
+        System.out.println("Add a drink");
+        System.out.println("Select drink size");
+        System.out.println("Select drink flavor");
     }
 
-    private void displayChipsScreen(){
-        System.out.println("chips screen working");
+    private void displayChipsScreen() {
+        System.out.println("Add chips");
+        System.out.println("Select chip flavor");
     }
 
-    private void displayCheckoutScreen(){
-
+    private void displayCheckoutScreen() {
+        //
+        System.out.println("checkout screen working");
     }
 
+    private void cancelOrder() {
+        System.out.println("cancel order method working");
+    }
 
+    private void displaySandwichSize() {
+        System.out.println("Select sandwich size: 4in, 8in, 12in");
+    }
+
+    private void displayBreadOptions() {
+        System.out.println("Select your bread option: white, wheat, rye, wrap");
+    }
+
+    private void displayPremiumToppings() {
+        System.out.println("Select your premium toppings");
+    }
+
+    private void displayRegularToppings() {
+        System.out.println("Select from the regular toppings")
+    }
+
+    private void displaySauceOptions() {
+        System.out.println("Select from the sauce options");
+    }
+
+    private void displayToastOptions() {
+        System.out.println("Select from toasted or untoasted");
+    }
+
+    private void displayReceiptScreen() {
+        System.out.println("Receipt display method working");
+    }
 }

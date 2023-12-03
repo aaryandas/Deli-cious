c05820a204926a7bba68be8ea1d475d4c683e8f5
Ren Das <aaryan.das.42gmail.com>
2023-12-03 04:56:28 -0600

    basic spring application

---
+++ ingredients/src/test/java/com/example/ingredients/IngredientsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ingredients;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IngredientsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
